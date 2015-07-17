77d5f761bb0fbc6850edcf1c6a33eafc2f643f1c
Nathaniel Swan <nathanieljswan@gmail.com>
2015-07-16 22:36:16 -0400

    implemented a status bar that will signify that the application is 'thinking'

--- Filex.java
+++ Filex.java
@@ -9,6 +9,8 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.Group;
 import javafx.scene.text.Text;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.ProgressIndicator;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.geometry.Insets;
@@ -18,37 +20,65 @@ import javafx.scene.control.TextField;
 
 public class Filex extends Application
 {
-    @Override
+
     public void start(final Stage stage) throws Exception
     {
-        final BorderPane borderpane = new BorderPane();
-        VBox searchPane = buildSearchPane();
-        borderpane.setLeft(searchPane);
-        final Scene scene = new Scene(borderpane, 720, 480);
-        
         stage.setTitle("Filex");
-        stage.setScene(scene);
+        stage.setScene(buildComponents());
         stage.show();
     }
 
-
-    public VBox buildSearchPane()
+    public Scene buildComponents()
     {
-        VBox searchPane = new VBox();
-        searchPane.setPadding(new Insets(10));
-        searchPane.setSpacing(8);
+        final BorderPane borderpane = new BorderPane();
 
+        VBox leftPane = new VBox();
+        leftPane.setPadding(new Insets(10));
+        leftPane.setSpacing(8);
+
+        Text title = getSearchTitle();
+        TextField searchBox = getSearchBox();
+        ProgressBar progressBar = getProgressBar(); 
+
+        leftPane.getChildren().addAll(title, searchBox, progressBar);
+        borderpane.setLeft(leftPane);
+
+
+        final Scene scene = new Scene(borderpane, 720, 480);
+        return scene;        
+    }
+
+
+    public Text getSearchTitle()
+    {
         Text title = new Text("Search");
         title.setFont(Font.font("Arial", FontWeight.BOLD, 14));
-        TextField query = new TextField();
-        searchPane.getChildren().addAll(title, query);
-        return searchPane;
+        return title;
     }
+
+    public TextField getSearchBox()
+    {
+        TextField query = new TextField();
+        return query;
+    }
+
+    public ProgressBar getProgressBar()
+    {
+        ProgressBar pb = new ProgressBar();
+        pb.setProgress(-1);
+        return pb;
+    }
+
     /*
      * Main function used to run Filex
      */ 
     public static void main(final String[] arguments)
     {
         Application.launch(arguments);
+    }
+
+    public void handleSearchPressed()
+    {
+
     }
 }
